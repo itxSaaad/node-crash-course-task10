17fd077dd82ff9e9d498a8c7e2ec9117a1e78d81
Muhammad Saad <saadstudent.cs@gmail.com>
2024-07-11 11:09:54 +0500

    Added: Simple Router

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import http from 'http';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>HomePage!</h1>');
+    res.end();
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>About Page!</h1>');
+    res.end();
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('<h1>Page Not Found!</h1>');
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
